18200848b37dbe3e4551d1826fcbc47b00826c2c
Thuan Le <gile@ucsc.edu>
2019-05-31 00:01:27 -0700

    Add some more skeletons for GET requests, remove version 1 of the pdf, add version 2. just want to play some smash bros

--- kvs.py
+++ kvs.py
@@ -64,6 +64,9 @@ def startup():
 # add to a shard with a fewest nodes first
 # i think the parameter is correct
 def add_to_shard(ip):
+    #TODO: So many todo :/
+    #position = 0
+    #shards[position].append(ip)
     pass
 
 # delete a node from a shard
@@ -71,10 +74,50 @@ def delete_from_shard(ip):
     pass
 
 # this only execute if CLIENT requests it
+# IDEA: remove a shard from a max node and add it into a min node
 def rebalance_shard(ip):
     pass
 
+# Add all the route here so we don't have to scroll
+# move it down when we are done :)
+# 4 GET requests from the PDF
+# Please double check it too
 
+#TODO
+# Get the shard IDs of the store
+@app.route(route_shard('/shard-ids'), methods=['GET'])
+def shard_id_get:
+    #http://<node-socket-address>/key-value-store-shard/shard-ids
+    #{"message":"Shard IDs retrieved successfully", "shard-ids":<shard-ids>} 200
+    # TODO
+    #return jsonify({
+    #   'message':'Shard IDs retrieved succesfully',
+    #   'shard-ids':shard_id
+    #   }),200
+    pass
+
+# Get the shard IDs of a node
+@app.route(route_shard('/node-shard-id'), methods=['GET'])
+def node_id_get:
+    #http://<node-socket-address>/key-value-store-shard/node-shard-id
+    #{"message":"Shard ID of the node retrieved successfully", "shard-id":<shard-id>} 200
+    pass
+
+# Get the members of a shard ID
+@app.route(route_shard('/shard-id-members/<shard-id>'), methods=['GET'])
+def member_id_get:
+    #http://<node-socket-address>/key-value-store-shard/shard-id-members/<shard-id>
+    #{"message":"Members of shard ID retrieved successfully", "shard-id-members":<shard-id-members>} 200
+    pass
+
+# Get the number of keys stored in a shard
+@app.route(route_shard('/shard-id-key-count/<shard-id>'), methods=['GET'])
+def key_get:
+    #http://<node-socket-address>/key-value-store-shard/shard-id-key-count/<shard-id>
+    #{"message":"Key count of shard ID retrieved successfully", "shard-id-key-count":<shard-id-key-count>} 200
+    pass
+
+############# Old Stuff#############3
 def pull_state(ip=None):
     global vector_clock
     global delivery_buffer
@@ -115,7 +158,8 @@ def pull_state(ip=None):
 
 def route(r=''):
     return '/key-value-store' + r
-
+def route_shard(r=''):
+    return '/key-value-store-shard' + r
 
 def broadcast_add_replica():
     return multicast(
